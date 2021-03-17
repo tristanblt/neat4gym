a2e919b9fd4019770554d937519a0a2a7e7e49b6
tristanblt <tristanblt@gmail.com>
2021-03-17 14:44:35 +0100

    🚧 Modify agent settings

--- gym4neatWrapper/src/Agent.cpp
+++ gym4neatWrapper/src/Agent.cpp
@@ -14,26 +14,32 @@ void Agent::run()
     std::vector<float> inputs;
     neat::NEAT::Data data;
     GymRequests::StepData step;
+    float fitness = 0;
 
     _gr.startMonitor(_instanceId);
 
     neat::NEAT neat(
-        1000,
+        5,
         _gr.observationSpace(_instanceId).n,
         _gr.actionSpace(_instanceId).n);
 
-    for (int episode = 0; episode < 150; episode++) {
-        std::cout << "Episode: " << episode << std::endl;
-        inputs = _gr.reset(_instanceId);
-        while (true) {
-            data = neat.step(step.isOver, step.score, inputs);
-            step = _gr.step(_instanceId, data.outputs);
-            inputs = step.inputs;
+    while(true) {
+        for (int episode = 0; episode < 5; episode++) {
+            inputs = _gr.reset(_instanceId);
+            while (true) {
+                data = neat.step(step.isOver, fitness, inputs);
+                step = _gr.step(_instanceId, data.outputs);
+                inputs = step.inputs;
 
-            if (step.isOver)
-                break;
+                fitness += step.score;
+
+                if (step.isOver) {
+                    std::cout << data.currentGeneration << "-" << (episode + 1) << "-> fitness: " << fitness << std::endl;
+                    fitness = 0;
+                    break;
+                }
+            }
         }
-        std::cout << "Generation: " << data.currentGeneration << std::endl;
     }
     _gr.closeMonitor(_instanceId);
 }
